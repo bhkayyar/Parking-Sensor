9e84c09b02a8ef46732ddb10052188a620426efc
Bharath <bharath_kayyar@brown.edu>
2020-12-12 13:09:10 -0800

    Update relay test:

--- src/write_to_relays/quad_relay_adapter.ino
+++ src/write_to_relays/quad_relay_adapter.ino
@@ -10,10 +10,12 @@ void turn_all_on() {
   turn_relay_on(GREEN_LIGHT);
   turn_relay_on(YELLOW_LIGHT);
   turn_relay_on(RED_LIGHT);
+  turn_relay_on(UNUSED_RELAY);
 }
 
 void turn_all_off() {
   turn_relay_off(GREEN_LIGHT);
   turn_relay_off(YELLOW_LIGHT);
   turn_relay_off(RED_LIGHT);
+  turn_relay_off(UNUSED_RELAY);
 }
--- src/write_to_relays/write_to_relays.ino
+++ src/write_to_relays/write_to_relays.ino
@@ -1,50 +1,37 @@
-#define GREEN_LIGHT 0
-#define YELLOW_LIGHT 1
-#define RED_LIGHT 2
+#define GREEN_LIGHT 2
+#define YELLOW_LIGHT 3
+#define RED_LIGHT 4
+#define UNUSED_RELAY 5
+
 #define DEBUG 1
 
 void setup() {
  pinMode(GREEN_LIGHT, OUTPUT);
  pinMode(YELLOW_LIGHT, OUTPUT);
  pinMode(RED_LIGHT, OUTPUT);
-}
-
-void turn_relay_on(int pin) {
-  digitalWrite(pin, LOW);
-}
-
-void turn_relay_off(int pin) {
-  digitalWrite(pin, HIGH);
-}
-
-void turn_all_on() {
-  turn_relay_on(GREEN_LIGHT);
-  turn_relay_on(YELLOW_LIGHT);
-  turn_relay_on(RED_LIGHT);
-}
-
-void turn_all_off() {
-  turn_relay_off(GREEN_LIGHT);
-  turn_relay_off(YELLOW_LIGHT);
-  turn_relay_off(RED_LIGHT);
+ pinMode(UNUSED_RELAY, OUTPUT);
 }
 
 void loop() {
   Serial.println("Entering sequence.");
   turn_relay_on(GREEN_LIGHT);
-  delay(500);
+  delay(1500);
   turn_relay_on(YELLOW_LIGHT);
-  delay(500);
+  delay(1500);
   turn_relay_on(RED_LIGHT);
   delay(1500);
+  turn_relay_on(UNUSED_RELAY);
+  delay(2500);
   turn_relay_off(GREEN_LIGHT);
-  delay(500);
+  delay(1500);
   turn_relay_off(YELLOW_LIGHT);
-  delay(500);
+  delay(1500);
   turn_relay_off(RED_LIGHT);
   delay(1500);
+  turn_relay_off(UNUSED_RELAY);
+  delay(2500);
   turn_all_on();
-  delay(1000);
+  delay(2000);
   turn_all_off();
   delay(2000);
   Serial.println("Exiting.");
